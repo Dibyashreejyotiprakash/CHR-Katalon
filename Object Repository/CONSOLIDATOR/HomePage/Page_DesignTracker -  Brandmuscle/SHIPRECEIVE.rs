40d25eeab40d76349b25a52d07abdabadec280f9
dibyajyotiprakash <dibyashree.jyoti@brandmuscle.com>
2019-05-20 17:11:26 +0530

    Removed unnecessary folders

--- Object Repository/CONSOLIDATOR/HomePage/Page_DesignTracker -  Brandmuscle/SHIPRECEIVE.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>SHIPRECEIVE</name>
-   <tag></tag>
-   <elementGuidId>d2d532f2-0a28-4bd5-94bf-6b071629a925</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
--- Object Repository/PROOFGALLERY/HomePage/Page_DesignTracker -  Brandmuscle/SHIPRECEIVE.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>SHIPRECEIVE</name>
-   <tag></tag>
-   <elementGuidId>409a83c1-144d-4943-91d1-703cd51bffca</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
