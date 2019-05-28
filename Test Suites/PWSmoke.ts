b5dba5402151be40c35ace8b76869d5b39370b26
dibyajyotiprakash <dibyashree.jyoti@brandmuscle.com>
2019-05-28 11:29:40 +0530

    Added IISmoke and DTSmoke Suites

---
+++ Test Suites/PWSmoke.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PWSmoke</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>de45b05b-efa8-4e48-81df-e89788815175</testSuiteGuid>
+</TestSuiteEntity>
