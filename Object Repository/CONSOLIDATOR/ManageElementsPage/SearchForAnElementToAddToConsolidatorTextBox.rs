02f88204d6017dc1d80d2274b1d8ed2436845730
Chandan Kumar <Chandan.Kumar@brandmuscle.com>
2019-05-30 14:24:23 +0530

    added consolidator base element smoke/regression cases

---
+++ Object Repository/CONSOLIDATOR/ManageElementsPage/SearchForAnElementToAddToConsolidatorTextBox.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>SearchForAnElementToAddToConsolidatorTextBox</name>
+   <tag></tag>
+   <elementGuidId>c7c5dd56-869b-46ea-8ca3-40cb484ed5ec</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id='searchExternalElementId']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
