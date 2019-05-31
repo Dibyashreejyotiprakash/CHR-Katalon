e178b85d8e6d19b6e2d354246bc88983e1fb6a95
Ashutosh Gupta <Ashutosh.Gupta@brandmuscle.com>
2019-05-31 11:56:04 +0530

    Added suite for consolidator regression

---
+++ Test Suites/ConsolidatorRegression.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ConsolidatorRegression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>94894c1a-fdfc-41d3-936c-3835ecdc41db</testSuiteGuid>
+</TestSuiteEntity>
