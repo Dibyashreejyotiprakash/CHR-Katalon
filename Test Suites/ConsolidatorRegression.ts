029bd2eb901d997eb371f0d339ae4eda6d7df6fc
Ashutosh Gupta <Ashutosh.Gupta@brandmuscle.com>
2019-05-31 12:43:50 +0530

    Added cases for consolidator regression

--- Test Suites/ConsolidatorRegression.ts
+++ Test Suites/ConsolidatorRegression.ts
@@ -10,4 +10,34 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>94894c1a-fdfc-41d3-936c-3835ecdc41db</testSuiteGuid>
+   <testCaseLink>
+      <guid>b66817c4-6120-4fc4-91ae-adeb93023db9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CSPW/CS REGRESSION/ManageBaseElementAddNewItem</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>24948dec-6e87-474e-9092-13c7ce0f3b4b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CSPW/CS REGRESSION/ManageBaseElementDelete</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b22c867d-07f9-4840-af5d-78b41234029d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CSPW/CS REGRESSION/ManageBaseElementEdit</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>23b75eeb-d6c3-402b-a34d-6f5783d05687</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CSPW/CS REGRESSION/ManageBaseElementSearch</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5fd51f06-9eb4-4539-8ecb-7a6b2215492c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CSPW/CS REGRESSION/VerifyManageBaseElementCoulmnSort</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
