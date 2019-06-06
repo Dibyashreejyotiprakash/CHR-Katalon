73d92d9fce01313c7d6e0a471df49383411cfd85
Astha Nigam <Astha.Nigam@brandmuscle.com>
2019-06-06 12:39:23 +0530

    Edit/Delete Smoke

---
+++ Object Repository/II-WSWADMIN/MetatagEditDelete/CorporationFilter.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>CorporationFilter</name>
+   <tag></tag>
+   <elementGuidId>9a96d8aa-5e39-4d30-887b-8d802fef4978</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//input[@class = 'rgFilterBox'])[1]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
