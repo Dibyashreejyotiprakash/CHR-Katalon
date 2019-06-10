6c54baa8ec39c5447ebd70275a61cbec1bf3d8b8
Astha Nigam <Astha.Nigam@brandmuscle.com>
2019-06-10 11:36:34 +0530

    adding bulk meta tag

---
+++ Object Repository/II-WSWADMIN/BulkMetaTaggingPage/ItemTypeDropDownBulkPage.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ItemTypeDropDownBulkPage</name>
+   <tag></tag>
+   <elementGuidId>d3ec5302-41a3-453c-bbcc-ce2ce5897a30</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@id='cphMain_cphMain_DdlItemType']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/II-WSWADMIN/MainMenu/MetaTaggingBulkImportPage.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>MetaTaggingBulkImportPage</name>
+   <tag></tag>
+   <elementGuidId>11be784e-9eb7-47ab-8869-17c83d5000c1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//span[text()='Meta Tagging Bulk Import Tool'])[1]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
