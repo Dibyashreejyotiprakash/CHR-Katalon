718e90f4ee2968590e22d7103bff91c561b46209
Astha Nigam <Astha.Nigam@brandmuscle.com>
2019-06-11 08:31:59 +0530

    added metatags objects for bulk upload page

---
+++ Object Repository/II-WSWADMIN/BulkMetaTaggingPage/ContinueBtn.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ContinueBtn</name>
+   <tag></tag>
+   <elementGuidId>1b37fb4c-e46a-4778-b319-fa0d3c3784a8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[@id = 'cphMain_cphMain_glyphicon']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
