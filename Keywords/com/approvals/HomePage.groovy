af44038f0ef4888a0ae7cd3b75ad0f1685a49b4b
Ashutosh Gupta <Ashutosh.Gupta@brandmuscle.com>
2020-01-03 06:54:06 +0000

    Conflict resolutions for !22027

--- Keywords/com/approvals/HomePage.groovy
+++ Keywords/com/approvals/HomePage.groovy
@@ -185,7 +185,6 @@ public class HomePage {
 	public void VerifyDTSearchByJobName(String givenjobname) {
 		try{
 			action.Click(statusddn)
-			action.WaitVisibleDup(createdstatus)
 			action.Click(createdstatus)
 			action.Type(jobname, givenjobname)
 			action.Click(searchbtn)
@@ -207,7 +206,6 @@ public class HomePage {
 	@Keyword
 	public void VerifyDTJobsAfterSearchBySalesperson() {
 		try{
-
 			action.Click(salespersonddn)
 			action.Click(salespersonddnvalue)
 			action.Click(searchbtn)
